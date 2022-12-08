13b636c17d3dbcbc0e4e4f83e515067446ec03f0
Eduardo Hernández Soto <hseduardo@gmail.com>
2022-12-07 18:25:29 -0600

    component objective

--- src/components/home/Objective.jsx
+++ src/components/home/Objective.jsx
@@ -1,13 +1,15 @@
-const Objective = ({ img, paragraph, listed, elements}) => {
+const Objective = ({ img, paragraph, listed, elements }) => {
   return (
     <div class="card__objectives">
       <img src={img} alt="goal image" class="card__image" />
       <p class="text__objetives">{paragraph}</p>
-      {listed
-        ? elements.map((value, index) => {
-            return <li key={index}>{value}</li>;
-          })
-        : listed}
+      {listed && (
+        <ul>
+          {elements.map((list, index) => {
+            return <li key={index}>{list}</li>;
+          })}
+        </ul>
+      )}
     </div>
   );
 };
